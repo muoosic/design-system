679c567609b9ab33dd2c91ff8e8f1f126f2e4263
izakdvlpr <izakdvlpr@gmail.com>
2022-10-25 16:11:11 -0300

    fix: Add subpath to Storybook on production

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/muoosic-design-system/';
+      config.base = '/design-system/';
     }
 
     return config;
--- packages/docs/vite.config.js
+++ packages/docs/vite.config.js
@@ -1,6 +1,6 @@
-import vite from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
-export default vite.defineConfig({
+export default defineConfig({
   plugins: [react()],
 });
